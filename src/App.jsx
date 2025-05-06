8efdea55ed56916eeb5cd8e3f114ecd858ea18a6
mvplayea <cjllakv@gmail.com>
2025-05-06 21:14:34 +0700

    setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import Navbar from "./components/Navbar";
+import "./App.css";
+import Button from "./components/Button";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
+      <Navbar />
+      <Button color="blue">Click 1</Button>
+      <Button>Click 2</Button>
+      <Button>Click 3</Button>
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -29,7 +35,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Button.jsx
@@ -0,0 +1,3 @@
+export default function Button({ children, color }) {
+  return <button style={{ color: color }}>{children}</button>;
+}
