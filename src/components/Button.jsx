9546fea5e5d7238ed1cce1e302d61d35af52cf09
Galih Adhi Kusuma <84978361+NoxturneDev@users.noreply.github.com>
2025-05-07 09:20:13 +0700

    fixed tailwind dynamic class

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -8,10 +8,18 @@ const Button = ({
   color = "blue",
   ...props
 }) => {
+  const buttonStyleColor = {
+    blue: "bg-blue-500 hover:bg-blue-600",
+    purple: "bg-purple-500 hover:bg-purple-600",
+    red: "bg-red-500 hover:bg-red-600",
+    green: "bg-green-500 hover:bg-green-600",
+    yellow: "bg-yellow-500 hover:bg-yellow-600",
+  }
+
   return (
     <button
       type={type}
-      className={`rounded-md font-medium transition-colors focus:outline-none focus:ring-2 focus:ring-opacity-50 bg-${color}-500 hover:bg-${color}-600 text-white px-5 py-2 ${className}`}
+      className={`rounded-md font-medium transition-colors focus:outline-none focus:ring-2 focus:ring-opacity-50 ${buttonStyleColor[color]} text-white px-5 py-2 ${className}`}
       onClick={onClick}
       {...props}
     >
